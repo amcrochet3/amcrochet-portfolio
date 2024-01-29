fc3106e09466d0ab3b753bcde943a05c44fd1987
Allison Crochet <acrochet794@gmail.com>
2024-01-29 02:26:04 -0500

    Fixed broken LinkedIn link

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                     <div className='mt-5'>
                         <p className='text-left intro'>Based in <strong>Savannah, Georgia, USA</strong> 🌅🏝️🌊⛵🦐🐠🐙🐬🐚🩴</p>
                         <p className='text-left intro'>After nearly a decade in academia and creative roles, I decided in Spring 2023 to explore coding through <strong>Hackbright Academy's immersive software engineering fellowship</strong>. Originally wanting to gain a solid programming background to complement my <strong>M.S. in UX Design from Georgia Tech</strong>, I became enthralled by the limitless possibilities underlying the ability to craft digital products from a technological standpoint. So, I decided to leverage my interdisciplinary background in making a full pivot into software engineering! Below are some projects I’ve completed since graduating from Hackbright. I love working with data and delving into <strong>APIs</strong>. Recently, I am interested in <strong>machine learning</strong> and <strong>data science</strong> concepts.</p>
-                        <p className='text-left intro'>Feel free to check out my <strong><a href='https://github.com/amcrochet3'>GitHub</a></strong> and reach out to me on <strong><a href='https://linkedin.com/allison-crochet'>LinkedIn</a></strong> 🌱</p>
+                        <p className='text-left intro'>Feel free to check out my <strong><a href='https://github.com/amcrochet3'>GitHub</a></strong> and reach out to me on <strong><a href='https://linkedin.com/in/allison-crochet'>LinkedIn</a></strong> 🌱</p>
                     </div>
                 </div>
             </div>
